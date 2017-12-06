c6543e9f698d79249cef1ee63f7119d64cfabf2f
Austin <steeleshellsword@gmail.com>
2017-12-06 05:04:31 +0000

    Added file upload to Active Admin, not fuctional.

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,4 +1,8 @@
 class Picture < ApplicationRecord
-  has_many :technical_machines
   has_attached_file :image, styles: { small: "64x64", med: "100x100", large: "200x200" }
+
+  validates_attachment :image, :presence => true,
+     :content_type => { :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"] },
+     :size => { :in => 0..1000.kilobytes }
+  has_many :technical_machine
 end
