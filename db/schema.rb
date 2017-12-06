56a9d6305adcb20be40930e63492b85b61d892ef
Austin <steeleshellsword@gmail.com>
2017-12-06 02:43:05 +0000

    Migrated DB to add Pictures.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171123095040) do
+ActiveRecord::Schema.define(version: 20171206023844) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
@@ -94,6 +94,17 @@ ActiveRecord::Schema.define(version: 20171123095040) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "status"
+  end
+
+  create_table "pictures", force: :cascade do |t|
+    t.string "title"
+    t.string "body"
+    t.string "image_file_name"
+    t.string "image_content_type"
+    t.integer "image_file_size"
+    t.datetime "image_updated_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "provinces", force: :cascade do |t|
