ebe5d0ccb3243dce54513298ab19a3b31b2b8e03
Austin <steeleshellsword@gmail.com>
2017-12-07 11:09:31 +0000

    DB Migrate fix.

--- db/migrate/20171119232944_create_information.rb
+++ db/migrate/20171119232944_create_information.rb
@@ -1,6 +1,5 @@
 class CreateInformation < ActiveRecord::Migration[5.1]
   def change
-    drop_table :information
     create_table :information do |t|
       t.text :description
       t.string :postal_code
