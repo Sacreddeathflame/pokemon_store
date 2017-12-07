08dc53cdbe0ddc90dd7fc32b2cf0ed07df66aeb4
Austin <steeleshellsword@gmail.com>
2017-12-07 08:08:24 +0000

    Fixed up quotations in technical_machine model.

--- app/models/technical_machine.rb
+++ app/models/technical_machine.rb
@@ -11,18 +11,18 @@ class TechnicalMachine < ApplicationRecord
   validates :type_id, :status, presence: true
 
   def self.search(search, filter)
-    where("name LIKE ? OR description LIKE ? AND status LIKE ?", "%#{search}%", "%#{search}%", "%#{filter}%")
+    where('name LIKE ? OR description LIKE ? AND status LIKE ?', "%#{search}%", "%#{search}%", "%#{filter}%")
   end
 
   def self.type_search(type, filter)
-    where("type_id = ? AND status LIKE ?", "#{type}", "%#{filter}%")
+    where('type_id = ? AND status LIKE ?', "#{type}", "%#{filter}%")
   end
 
   def self.full_search(search, type, filter)
-    where("name LIKE ? OR description LIKE ? AND type_id = ? AND status LIKE ?", "%#{search}%", "%#{search}%", "#{type}", "%#{filter}%")
+    where('name LIKE ? OR description LIKE ? AND type_id = ? AND status LIKE ?', "%#{search}%", "%#{search}%", "#{type}", "%#{filter}%")
   end
 
   def self.all_search(filter)
-    where("status LIKE ?", "#{filter}")
+    where('status LIKE ?', "#{filter}")
   end
 end
